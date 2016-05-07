30aea5f7ad1e1cb454ae26c3fdc00ec64f9609df
David-solar <davisolar94@gmail.com>
2016-05-06 22:23:43 -0500

    Se cambio nombre del proyecto y se agrego activity SOBREELDESARROLLADOR

---
+++ SubnettingApp/app/src/main/java/com/desmov/subneteo/subnettingapp/Bienvenido.java
@@ -0,0 +1,100 @@
+package com.desmov.subneteo.subnettingapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class Bienvenido extends AppCompatActivity implements View.OnClickListener{
+
+
+    Button subneteo;
+    Button sobreDesarrollador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        subneteo = (Button) findViewById(R.id.btnSubneteo);
+        sobreDesarrollador = (Button) findViewById(R.id.btnSobreDesarrollador);
+
+        subneteo.setOnClickListener(this);
+        sobreDesarrollador.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bienvenido, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        switch (id)
+        {
+            case R.id.action_settings:
+                break;
+
+            case R.id.acercaDe:
+                break;
+
+            case R.id.conceptosBasicos:
+                break;
+
+            case R.id.que:
+                break;
+
+            case R.id.utilidad:
+                break;
+
+            case R.id.ayuda:
+                break;
+
+            case R.id.contactanos:
+                break;
+
+            case R.id.opiniones:
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public void onClick(View v)
+    {
+
+
+
+    }
+}
