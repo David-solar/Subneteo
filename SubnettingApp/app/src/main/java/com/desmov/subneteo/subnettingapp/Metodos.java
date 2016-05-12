52ac43b0efc388e7f3d1371853ebc181615cc299
David-solar <davisolar94@gmail.com>
2016-05-12 07:25:13 -0500

    se termino primer caso de aumento en 4to hocteto

--- SubnettingApp/app/src/main/java/com/desmov/subneteo/subnettingapp/Metodos.java
+++ SubnettingApp/app/src/main/java/com/desmov/subneteo/subnettingapp/Metodos.java
@@ -22,6 +22,15 @@ public class Metodos
     public static ArrayList<Integer> prefijos = new ArrayList<>();
     public static ArrayList<Integer> ip = new ArrayList<>();
 
+    public static ArrayList<String> tablaSubneteo = new ArrayList<>();
+
+    public static ArrayList<String> lineaPrefijo = new ArrayList<>();
+
+    public int ipSig = 0;
+    public int mascara = 0;
+
+    int[] bin = {128, 64, 32, 16, 8, 4, 2, 1};
+
     public void ordenarPrefijos()
     {
         for(int i = 0; i < prefijos.size()-1; i++)
@@ -92,6 +101,94 @@ public class Metodos
 
 
 
+    public void calcularSubneteo()
+    {
+
+        for(int i = 0; i < prefijos.size(); i++)
+        {
+
+            int noHocteto = (prefijos.get(i)/8) + 1;
+
+            if(i == 0)
+            {
+                String ipCompleta =  ip.get(0) + "." + ip.get(1) + "." + ip.get(2) + "." + ip.get(3);
+
+
+                lineaPrefijo.add(prefijos.get(0).toString());
+                lineaPrefijo.add(ipCompleta);
+                lineaPrefijo.add( ip.get(0) + "." + ip.get(1) + "." + ip.get(2) + "." + (ip.get(3) + 1) );
+
+                sumaHocteto(noHocteto, prefijos.get(0));
+
+            }
+        }
+    }
+
+    public void sumaHocteto(int noHocteto, int pref)
+    {
+        if(noHocteto == 1)
+        {
+
+        }
+        else if(noHocteto == 2)
+        {
+
+        }
+        else if(noHocteto == 3)
+        {
+
+        }
+        else if(noHocteto == 4)
+        {
+
+            ipSig = calcularMascaraYAumento(ip.get(3), pref);
+            String ipfinal = "";
+            String breadcast = "";
+            String masc = "";
+
+            if(ipSig == 0)
+            {
+
+            }
+            else
+            {
+                ipfinal = ip.get(0) + "." + ip.get(1) + "." + ip.get(3) + "." + (ipSig-2);
+                breadcast = ip.get(0) + "." + ip.get(1) + "." + ip.get(3) + "." + (ipSig-1);
+                masc = "255.255.255." + mascara;
+            }
+
+
+
+
+        }
+
+    }
+
+
+    public int calcularMascaraYAumento(int hoct, int pref)
+    {
+        mascara = 0;
+        int j = 0;
+        int no = 0;
+
+        for(int i = 0; i < pref; i++, j++)
+        {
+            if( (j%8)==0 && j>0 )
+            {
+                j = 0;
+                mascara = 0;
+            }
+            if(i == (pref-1) )
+            {
+                no = bin[j];
+            }
+            mascara = mascara + bin[j];
+
+        }
+
+        return (no + hoct);
+
+    }
 
 
 }
